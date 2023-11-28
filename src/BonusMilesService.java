55081adcf0d33cde11d77dd8f992b4c70b3988a2
vener <vener.gazin@gmail.com>
2023-11-28 19:10:53 +0300

    reformate code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
     public int calculate(int price) {
         int miles;
-        int percent=5;
-        miles = price*percent / 100;
+        int percent = 5;
+        miles = price * percent / 100;
         return miles;
     }
 }
